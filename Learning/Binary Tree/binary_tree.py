4513a27acace9044129a6df70abf7ae6261c4d29
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-10 08:08:47 +0530

    Added binary_tree.py

---
+++ Learning/Binary Tree/binary_tree.py
@@ -0,0 +1,61 @@
+class TreeNode:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.right = right
+        self.left = left
+        
+    def __str__(self):
+        return str(self.val)
+        
+#       1
+#   2       3
+# 4   5   10
+
+a = TreeNode(1)
+b = TreeNode(2)
+c = TreeNode(3)
+d = TreeNode(4)
+e = TreeNode(5)
+f = TreeNode(10)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+
+
+# Recursive Pre Order Traversal (DFS) Time: O(N) Space: O(N)
+
+def pre_order(node):
+    # This is the base case. When the right or left is None we just return.
+    if not node:
+        return
+    
+    print(node)
+    pre_order(node.left)
+    pre_order(node.right)
+    
+# pre_order(a)
+
+def in_order(node):
+    # This is the base case. When the right or left is None we just return.
+    if not node:
+        return
+
+    in_order(node.left)
+    print(node)
+    in_order(node.right)
+    
+# in_order(a)
+
+def post_order(node):
+    # This is the base case. When the right or left is None we just return.
+    if not node:
+        return
+
+    post_order(node.left)
+    post_order(node.right)
+    print(node)
+    
+post_order(a)
