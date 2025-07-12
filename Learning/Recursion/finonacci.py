eea087fe49f4f8f51de3ced057419058dbd48759
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-12 10:19:44 +0530

    Added Top-Down with Memoization (a.k.a. Recursion + Memoization)

--- Learning/Recursion/finonacci.py
+++ Learning/Recursion/finonacci.py
@@ -1,6 +1,19 @@
-def fibo(n):
+def fibo_1(n):
     if n <= 1:
         return n
-    return fibo(n - 1) + fibo(n - 2)
+    return fibo_1(n - 1) + fibo_1(n - 2)
     
-print(fibo(5))
+print(fibo_1(6))
+
+
+
+def fibo(n):
+    cache = {0:0, 1:1}
+    def fibonacci(n):
+        if n not in cache:
+            cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        return cache[n]
+    return fibonacci(n)
+
+
+print(fibo(6))
