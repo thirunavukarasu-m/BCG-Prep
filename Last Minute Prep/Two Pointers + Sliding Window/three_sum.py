5b0f525352709cafb4c406793677fd34a293795e
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-16 23:01:46 +0530

    Added three sum to LMP.

---
+++ Last Minute Prep/Two Pointers + Sliding Window/three_sum.py
@@ -0,0 +1,34 @@
+def three_sum(arr, target=None):
+    if len(arr) < 3:
+        return []
+    arr.sort()
+    initial_position = 0
+    result = []
+    while initial_position < len(arr) - 2:
+        if initial_position > 0 and arr[initial_position] == arr[initial_position - 1]:
+            initial_position += 1
+            continue
+        left, right = initial_position + 1,len(arr) - 1
+        while left < right:
+            current_sum = arr[initial_position] + arr[left] + arr[right]
+            if current_sum == 0:
+                result.append([arr[initial_position] , arr[left] , arr[right]])
+                
+                while left < right and arr[left] == arr[left + 1]:
+                    left += 1
+                while left < right and arr[right] == arr[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+            elif current_sum > 0:
+                right -= 1
+            else:
+                left += 1
+        initial_position += 1
+        
+    
+    return result
+print(three_sum([2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10], 0))
+
+
+[[-10,5,5],[-5,0,5],[-4,2,2],[-3,-2,5],[-3,1,2],[-2,0,2]]
