cf9b59733342a247af5ed2f0b517af620b874e8c
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-17 10:09:15 +0530

    Added LCA of BST in LMP.

---
+++ Last Minute Prep/Trees/lca_of_bst.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        lca = [root]
+
+        def search(root):
+            if not root:
+                return
+
+            lca[0] = root
+
+            if root is p or root is q:
+                return
+            elif root.val > p.val and root.val > q.val:
+                search(root.left)
+            elif root.val < p.val and root.val < q.val:
+                search(root.right)
+            else:
+                return
+
+        search(root)
+        return lca[0]
+        
